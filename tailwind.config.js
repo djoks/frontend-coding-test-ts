49eb20ad11f6c9d3e763fd4efb9514bfadebe43a
djoks <contact.djoks@gmail.com>
2024-01-16 02:33:35 +0000

    feat: add pokeball assets.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pokeball': "url('@/assets/pokeball-light.png')",
+      },
+    },
   },
   plugins: [],
 }
