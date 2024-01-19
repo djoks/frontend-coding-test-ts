6551aaa2523ed31cb336c3c3139445ea3ba81d2d
djoks <contact.djoks@gmail.com>
2024-01-19 00:13:19 +0000

    feat: create component to render list of pokemon.

---
+++ src/components/PokeList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-3 md:gap-5">
+        <poke-tile v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Pokemon } from '@/types/pokemon';
+import { PokeTile } from '@/components';
+
+defineProps<{ pokemons: Pokemon[] }>();
+
+</script>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import AppLogo from './AppLogo.vue'
 import AppMenu from './AppMenu.vue'
 import PokeTile from './PokeTile.vue'
+import PokeList from './PokeList.vue'
 
-export { AppLogo, AppMenu, PokeTile }
+export { AppLogo, AppMenu, PokeTile, PokeList }
